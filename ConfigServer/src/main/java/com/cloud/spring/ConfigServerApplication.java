c14e3468a8f1ad5a09613aa24591e222b3e91cce
Pulkit Gupta <pulgupta@gmail.com>
2016-07-09 11:30:14 +0530

    added

---
+++ ConfigServer/src/main/java/com/cloud/spring/ConfigServerApplication.java
@@ -0,0 +1,19 @@
+package com.cloud.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Server available at http://localhost:8888/configclient/default
+ * @author pulgupta
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+}
---
+++ reservation/src/main/java/com/example/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope // To refresh in case the configuration changes
+@RestController
+public class HelloWorld {
+
+	@Value("${message.greeting}")
+	String name="Gargi";
+	
+	@RequestMapping("/greeting")
+	public String helloWorld() {
+		return "Hello " + name;
+	}
+}
