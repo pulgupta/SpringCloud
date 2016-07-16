b03fe7f138ddbe69353b1c70fa080fad57704e78
Pulkit Gupta <pulgupta@gmail.com>
2016-07-16 11:03:25 +0530

    added auto refreh in config server

--- ProxyServer/src/main/java/com/example/ProxyServerApplication.java
+++ ProxyServer/src/main/java/com/example/ProxyServerApplication.java
@@ -3,9 +3,7 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 
 @EnableZuulProxy
 @EnableDiscoveryClient
