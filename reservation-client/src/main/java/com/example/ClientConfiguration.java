08c9e1e10d4f206bb431d68ddade6e8ebe7280e3
Pulkit Gupta <pulgupta@gmail.com>
2016-11-20 20:44:56 +0530

    many changes

--- reservation-client/src/main/java/com/example/ClientConfiguration.java
+++ reservation-client/src/main/java/com/example/ClientConfiguration.java
@@ -5,6 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+/**
+ * To so that you call know rest template is thread safe so its perfectly fine
+ * to pass it on to different threads and see the benefits of successive calls
+ * @author pulgupta
+ *
+ */
 @Configuration
 public class ClientConfiguration {
 
